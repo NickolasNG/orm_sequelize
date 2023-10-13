bbb36808341b2b695e118361a9e328ac3232e167
Destroier007 <nickolas.ng@hotmail.com>
2023-10-13 12:17:10 -0300

    Fazendo associações

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,6 +3,10 @@ const PessoaController = require('../controllers/PessoaController.js');
 
 const router = Router()
 
-router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', PessoaController.pegoUmaPessoa);
+router.post('/pessoas', PessoaController.criaPessoa);
+router.put('/pessoas/:id', PessoaController.atualizaPessoa);
+router.delete('/pessoas/:id', PessoaController.apagaPessoa);
 
 module.exports = router
