56f0240a8f3cb10f504da20575a4fca5ddfb1dd5
Destroier007 <nickolas.ng@hotmail.com>
2023-10-17 12:59:28 -0300

    Mais controladores e rotas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,13 @@
 const bodyParser = require('body-parser');
 const pessoas = require('./pessoasRoute.js')
+const niveis = require('./niveisRoute.js')
+const turmas = require('./turmaRoute')
 
 module.exports = app => {
-    app.use(bodyParser.json())
-    app.use(pessoas)
+  app.use(
+    bodyParser.json(),
+    pessoas,
+    niveis,
+    turmas
+  )
 }
